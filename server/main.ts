563920020d9f134f2c0ad372019d478a3e42a183
郭永杰 <guoyongjie@sunmi.com>
2019-06-14 12:09:24 +0800

    feat(secure): 添加安全相关设置

--- server/main.ts
+++ server/main.ts
@@ -1,12 +1,20 @@
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ENV } from '@config/env';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { HttpExceptionFilter } from './common/filters/http-expection';
+// import { HttpExceptionFilter } from './common/filters/http-expection';
 import { NestFactory } from '@nestjs/core';
 import { NextHandlerFilter } from './common/filters/next-handler';
+import { ValidationPipe } from '@nestjs/common';
+// import csurf from 'csurf';
 import express from 'express';
+import helmet from 'helmet';
+import { join } from 'path';
 import { logger } from '@core/logger';
 import next from 'next';
+import rateLimit from 'express-rate-limit';
 import session from 'express-session';
 
 async function bootstrap() {
@@ -14,12 +22,16 @@ async function bootstrap() {
   await nextApp.prepare();
 
   const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(server),
+  );
 
+  app.useGlobalPipes(new ValidationPipe());
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setGlobalPrefix('api');
-  app.useGlobalFilters(new HttpExceptionFilter());
+  // app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalFilters(new NextHandlerFilter(nextApp));
-
   // 会话设置
   app.use(
     session({
@@ -40,7 +52,16 @@ async function bootstrap() {
       // store: ,
     }),
   );
-
+  // somewhere in your initialization file
+  app.use(helmet());
+  // app.use(csurf());
+  app.use(
+    new rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 10000, // limit each IP to 100 requests per windowMs
+    }),
+  );
+  // TODO:
   // 跨域安全
   app.enableCors({
     origin: [/\.sunmi\.com$/],
--- server/user/user.controller.ts
+++ server/user/user.controller.ts
@@ -1,13 +1,23 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from '../user/user.service';
 
 @Controller('user')
-@UseGuards(AuthGuard('jwt'))
 export class UserController {
   constructor(private readonly service: UserService) {}
 
   @Get('sync')
+  @UseGuards(AuthGuard('jwt'))
   public async sync() {
     const json = await import('./data/user.json');
     return Promise.all(
@@ -21,17 +31,21 @@ export class UserController {
     );
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get()
+  @UseGuards(AuthGuard('jwt'))
   public async findAll() {
     return this.service.findAll();
   }
 
   @Post()
-  public async create(@Body() body: any) {
+  public async create(@Body() body: CreateUserDto) {
     return await this.service.create(body);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get(':id')
+  @UseGuards(AuthGuard('jwt'))
   public async findOne(@Param('id') id: string) {
     return this.service.findOne(id);
   }
