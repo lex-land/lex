c0d85fe601ad5e6069ff8aa6a397592cb9ceae2f
briefguo <gyj9094@gmail.com>
2019-06-24 13:55:32 +0800

    feat(inte): 去掉core/tree-editor

--- pages/repositories/modules/interface.tsx
+++ pages/repositories/modules/interface.tsx
@@ -1,6 +1,7 @@
-import { Button, ButtonGroup, H2, H3, H4 } from '@blueprintjs/core';
+import { Button, ButtonGroup, H1, H3, H4 } from '@blueprintjs/core';
 import React, { Fragment } from 'react';
-import { TreeEditor, TreeUtil } from '@core/tree-editor';
+import { TreeEditor, TreeUtil } from '@components/domains/inte';
+import { CURD } from '@components/curd';
 import { Flex } from '@components/layout/flex';
 import { Interface } from '@server/interface/interface.entity';
 import { Page } from '@components/page';
@@ -30,39 +31,53 @@ export default () => {
     <Page>
       <Page.Navbar />
       <Repo.SubPage>
-        <Repo.Nav />
-        <Flex.Auto>
-          <div>
-            <H2 className="page-title">
-              [{inte.method}] {inte.url}({inte.name})
-            </H2>
-            {inte.description && <p>{inte.description}</p>}
-          </div>
-          <div style={{ padding: '40px 0' }}>
-            <H4>请求参数</H4>
-            <TreeEditor
-              header={TreeEditorHeader}
-              inte={inte}
-              scope="request"
-              maxDepth={1}
-              editable={false}
-              defaultValue={inte.properties.filter(p => p.scope === 'request')}
+        <Flex>
+          <Repo.Sider />
+          <Page.Content>
+            <div>
+              <H1>{inte.name}</H1>
+              {inte.description && <p>{inte.description}</p>}
+              <p>
+                [{inte.method}] {inte.url}
+              </p>
+            </div>
+            <CURD.Update
+              action={`/api/interface/${inte.id}`}
+              defaultValue={inte}
+              fields={['name', 'description', 'method', 'url']}
+              successForceReload
+              button={<CURD.Button text="编辑" />}
             />
-          </div>
-          <div style={{ padding: '40px 0' }}>
-            <H4>响应参数</H4>
-            <TreeEditor
-              header={TreeEditorHeader}
-              inte={inte}
-              scope="response"
-              editable={false}
-              defaultValue={inte.properties.filter(p => p.scope === 'response')}
-            />
-          </div>
-          <div>
-            <H3>返回码说明</H3>
-          </div>
-        </Flex.Auto>
+            <div style={{ padding: '40px 0' }}>
+              <H4>请求参数</H4>
+              <TreeEditor
+                header={TreeEditorHeader}
+                inte={inte}
+                scope="request"
+                maxDepth={1}
+                editable={false}
+                defaultValue={inte.properties.filter(
+                  p => p.scope === 'request',
+                )}
+              />
+            </div>
+            <div style={{ padding: '40px 0' }}>
+              <H4>响应参数</H4>
+              <TreeEditor
+                header={TreeEditorHeader}
+                inte={inte}
+                scope="response"
+                editable={false}
+                defaultValue={inte.properties.filter(
+                  p => p.scope === 'response',
+                )}
+              />
+            </div>
+            <div>
+              <H3>返回码说明</H3>
+            </div>
+          </Page.Content>
+        </Flex>
       </Repo.SubPage>
     </Page>
   );
