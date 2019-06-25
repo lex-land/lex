7f8db071cafaf9be33e5ca501875135e8b0abed1
briefguo <gyj9094@gmail.com>
2019-06-25 22:07:54 +0800

    feat(orgs): 组织新建仓库

--- pages/orgs/repositories/new.tsx
+++ pages/orgs/repositories/new.tsx
@@ -1,20 +1,23 @@
+import { composePageProps, usePageProps } from '@core/next-compose';
 import { H1 } from '@blueprintjs/core';
 import { Page } from '@components/page';
 import { QuickForm } from '@components/forms';
 import React from 'react';
-import { composePageProps } from '@core/next-compose';
 import { http } from '@helpers/fetch';
-// import { org } from '@helpers/page-props';
+import { org } from '@helpers/page-props';
 import { route } from '@helpers/route';
 
-export default composePageProps()(() => {
+export default composePageProps(org)(() => {
+  const { org } = usePageProps();
   return (
     <Page>
       <Page.Navbar />
       <Page.Content>
         <H1>创建一个仓库</H1>
         <QuickForm
-          action={newValue => http.post('/api/repository', newValue)}
+          action={newValue =>
+            http.post('/api/repository', { ...newValue, organization: org })
+          }
           fields={['name', 'description']}
           success={(values, json) =>
             route('repositories/show').replace({ repository_id: json.id })
