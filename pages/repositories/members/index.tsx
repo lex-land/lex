c05058b6ca34f282dd4f64eae6f36b9fd516235b
briefguo <gyj9094@gmail.com>
2019-06-28 19:07:52 +0800

    feat(member): member页面，orm的logger

--- pages/repositories/members/index.tsx
+++ pages/repositories/members/index.tsx
@@ -8,13 +8,14 @@ import { Page } from '@components/page';
 import { Repo } from '@components/domains/repo';
 import { Repository } from '@server/repository/repository.entity';
 import { User } from '@server/user/user.entity';
+import { http } from '@helpers/fetch';
 
 const MemberMultiSelect = MultiSelect.ofType<any>();
 
 export default composePageProps(repo, user.all)(() => {
   const { repo, users } = usePageProps<{ repo: Repository; users: User[] }>();
   const [selectedItems, setSelectedItems] = useState(
-    repo.members.map(i => ({ name: i.fullname })),
+    repo.members.map(i => ({ ...i, name: i.fullname })),
   );
   return (
     <Page>
@@ -28,32 +29,50 @@ export default composePageProps(repo, user.all)(() => {
               itemRenderer={(item, { handleClick }) => (
                 <MenuItem
                   icon={item.checked && 'tick'}
-                  key={item.name}
+                  key={item.id}
                   onClick={handleClick}
                   label={item.name}
                 />
               )}
               items={users.map(user => ({
+                id: user.id,
                 name: user.fullname,
-                checked: selectedItems.map(i => i.name).includes(user.fullname),
+                checked: selectedItems.map(i => i.id).includes(user.id),
               }))}
               noResults={<MenuItem disabled={true} text="No results." />}
               onItemSelect={item => {
                 item.checked = !item.checked;
-                if (selectedItems.map(i => i.name).includes(item.name)) {
+                if (selectedItems.map(i => i.id).includes(item.id)) {
                   // remove
                   selectedItems.splice(
-                    selectedItems.findIndex(i => i.name === item.name),
+                    selectedItems.findIndex(i => i.id === item.id),
                     1,
                   );
+                  http.delete(`/api/repository/${repo.id}/members`, item);
                   setSelectedItems([...selectedItems]);
                 } else {
                   // add
                   selectedItems.push(item);
+                  http.post(`/api/repository/${repo.id}/members`, item);
                   setSelectedItems([...selectedItems]);
                 }
               }}
-              tagRenderer={item => <span>{item.name}</span>}
+              tagRenderer={item => item.name}
+              tagInputProps={{
+                onRemove: (item, index) => {
+                  http.delete(
+                    `/api/repository/${repo.id}/members`,
+                    selectedItems[index],
+                  );
+                  selectedItems.splice(
+                    selectedItems.findIndex(
+                      i => i.id === selectedItems[index].id,
+                    ),
+                    1,
+                  );
+                  setSelectedItems([...selectedItems]);
+                },
+              }}
               selectedItems={selectedItems}
             />
           </Page.Content>
