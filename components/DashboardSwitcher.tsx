03ef68bd077568df789af06061b90e13ddc183d2
briefguo <gyj9094@gmail.com>
2019-07-19 11:43:15 +0800

    refactor(dashboard-switcher): 整理数据来源，使用useAsyncFn

--- components/DashboardSwitcher.tsx
+++ components/DashboardSwitcher.tsx
@@ -1,15 +1,18 @@
 import { Classes, Menu, MenuItem, Popover, Position } from '@blueprintjs/core';
-import React, { Fragment } from 'react';
 import { Avator } from '@/components/Avator';
+import Link from 'next/link';
+import React from 'react';
+import { User } from '@/interfaces/User';
+import cx from 'classnames';
+import httpHelper from '@/helpers/httpHelper';
 import styled from 'styled-components';
-// import { useAsync } from 'react-use';
+import { useAsyncFn } from 'react-use';
 
 const NavMenu = styled(Menu)`
   .bp3-menu-divider {
     border: 0;
     margin-left: -5px;
     margin-right: -5px;
-    /* margin-bottom: 16px; */
     background-image: linear-gradient(
       90deg,
       rgba(16, 22, 26, 0) 0,
@@ -25,44 +28,60 @@ const Container = styled.div`
   margin: 24px 0;
 `;
 
-// const defaultUser = {
-//   fullname: '-',
-//   joinedOrganizations: [],
-//   ownedOrganizations: [],
-// };
+const defaultUser = {
+  fullname: '',
+  joinedOrganizations: [],
+  ownedOrganizations: [],
+};
 
 export const DashboardSwitcher = ({ name }: { name: string }) => {
-  // const { value: user = defaultUser } = useAsync(getSessionUser);
-  // const orgs = user.joinedOrganizations.concat(user.ownedOrganizations);
+  const [{ value: sessionUser, loading }, fetchSession] = useAsyncFn(() =>
+    httpHelper.get<User>(`/api/session/user`),
+  );
+  const user = sessionUser || defaultUser;
+  const orgs = [...user.joinedOrganizations, ...user.ownedOrganizations];
+  const skeletonClass = cx({
+    [Classes.SKELETON]: loading,
+  });
+
   return (
     <Container>
       <Popover
         content={
-          <NavMenu className="dashboard-switcher__menu">
+          <NavMenu>
             <li className={Classes.MENU_HEADER}>
               <h6 className={Classes.HEADING}>Switch dashboard context</h6>
             </li>
-            <Fragment>
-              {/* <MenuItem href="/" icon="user" text={user.fullname} /> */}
-              {/* {orgs.map((org: any) => (
-                <MenuItem
-                  href={`/orgs/${encodeURIComponent(org.name)}`}
-                  icon="people"
-                  key={org.id}
-                  text={org.name}
-                />
-              ))} */}
-              <Menu.Divider />
-            </Fragment>
-            {/* {orgs.length > 0 && (
-              <Fragment>
-                <MenuItem href="/orgs" icon="cog" text="Manage orgnizations" />
+            <MenuItem
+              className={skeletonClass}
+              href="/"
+              icon="user"
+              text={user.fullname}
+            />
+            {orgs.map(org => (
+              <MenuItem
+                key={org.id}
+                className={skeletonClass}
+                href={`/orgs/${encodeURIComponent(org.name)}`}
+                icon="people"
+                text={org.name}
+              />
+            ))}
+            <Menu.Divider />
+            {orgs.length > 0 && (
+              <>
+                <Link href="/orgs">
+                  <MenuItem icon="cog" text="Manage orgnizations" />
+                </Link>
                 <Menu.Divider />
-              </Fragment>
-            )} */}
-            <MenuItem href="/orgs/new" icon="plus" text="Create orgnization" />
+              </>
+            )}
+            <Link href="/orgs/new">
+              <MenuItem icon="plus" text="Create orgnization" />
+            </Link>
           </NavMenu>
         }
+        onOpened={fetchSession}
         position={Position.BOTTOM_LEFT}
       >
         <Avator arrow label={name} text={name} />
