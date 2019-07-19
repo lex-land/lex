895debd336b28488d97d165b80f70da173dfca9d
briefguo <gyj9094@gmail.com>
2019-07-19 10:19:36 +0800

    fix(throttle): throttle 产生的fn引用不变，创建后便不可变

--- helpers/entityHelper.ts
+++ helpers/entityHelper.ts
@@ -2,8 +2,6 @@ import { useEffect, useState } from 'react';
 import httpHelper from './httpHelper';
 import { throttle } from 'lodash';
 
-export const createThrottled = (fn: any) => throttle(fn, 3000);
-
 export const updateEntityFn = (entity: string) => (id: number, query: any) => {
   httpHelper.put(`/api/${entity}/${id}`, query);
 };
@@ -17,7 +15,10 @@ export const getEntityFn = (entity: string) => (id: number) =>
 export const deleteEntityFn = (entity: string) => (id: number) =>
   httpHelper.delete(`/api/${entity}/${id}`);
 
-export const throttledUpdateEntityFn = createThrottled(updateEntityFn);
+// throttle 产生的fn引用不变，创建后便不可变
+// throttle(updateEntityFn)
+export const throttledUpdateEntityFn = (entity: string) =>
+  throttle(updateEntityFn(entity), 3000);
 
 export const useEntity = (entity: any, curd?: (newValue: any) => any) => {
   const [entityValue, setEntityValue] = useState(entity);
