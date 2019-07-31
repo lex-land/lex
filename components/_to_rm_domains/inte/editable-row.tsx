d3dbd57e01c2e282874074a7aaf49f8c2498ab45
briefguo <gyj9094@gmail.com>
2019-07-31 09:47:10 +0800

    feat(inte): 添加删除确认

--- components/_to_rm_domains/inte/editable-row.tsx
+++ components/_to_rm_domains/inte/editable-row.tsx
@@ -1,9 +1,12 @@
 import {
   Button,
   ButtonGroup,
+  Classes,
   Code,
   EditableText,
+  H5,
   HTMLSelect,
+  Intent,
   Popover,
   Switch,
   Tag,
@@ -139,12 +142,40 @@ export const EditableRow = (props: any) => {
             </div>
           </div>
           <ButtonGroup>
-            <Button
-              intent="danger"
-              onClick={props.onDelete}
-              minimal
-              icon="minus"
-            />
+            <Popover
+              content={
+                <div style={{ padding: '20px', maxWidth: 300 }} key="text">
+                  <H5>Confirm deletion</H5>
+                  <p>
+                    Are you sure you want to delete these items? You won't be
+                    able to recover them.
+                  </p>
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'flex-end',
+                      marginTop: 15,
+                    }}
+                  >
+                    <Button
+                      className={Classes.POPOVER_DISMISS}
+                      style={{ marginRight: 10 }}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      intent={Intent.DANGER}
+                      onClick={props.onDelete}
+                      className={Classes.POPOVER_DISMISS}
+                    >
+                      Delete
+                    </Button>
+                  </div>
+                </div>
+              }
+            >
+              <Button intent="danger" minimal icon="minus" />
+            </Popover>
             {rowHasCaret && (
               <Button
                 minimal
